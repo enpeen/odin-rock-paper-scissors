1bfa857d405415686765bcc3b06111c686a0e11b
enpeen <enpeen0@gmail.com>
2023-04-26 01:41:05 +0200

    Implementation of playRound function

--- javascript.js
+++ javascript.js
@@ -12,4 +12,34 @@ function getComputerChoice() {
     return result;
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (playerSelection !== "Rock" && playerSelection !== "Paper" && playerSelection !== "Scissors") {
+        alert("Please enter one of three options");
+    } else {
+        if (playerSelection === "Rock" && computerSelection === "Rock") {
+            return "Draw";
+        } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+            return "You lose. Paper beats Rock";
+        } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+            return "You win. Rock beats Scissors";
+        } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+            return "You win. Paper beats Rock";
+        } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+            return "Draw";
+        } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+            return "You lose. Scissors beats paper";
+        } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+            return "You lose. Rock beats Scissors";
+        } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+            return "You win. Scissors beats paper";
+        } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+            return "Draw";
+        }  
+    }
+} 
 
+let playerSelection = prompt("Game Rock Paper Scissors starts. Please enter your pick");
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
